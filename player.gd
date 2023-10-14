4c98943e659171283d34380b2bea4159cfe5e2b2
Albert Unmack Bærentzen <202104567@post.au.dk>
2023-10-14 15:01:54 +0200

    Changed some animations

---
+++ player.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+const SPEED = 600.0
+const JUMP_VELOCITY = -775.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var current_anim_sprite
+var current_coll_shape
+var can_jump
+var can_climb
+var is_climbing
+
+func _ready():
+	set_evolution("snail")
+	enable_current_evolution()
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor() and not is_climbing:
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if can_jump and Input.is_action_pressed("move_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if can_climb and Input.is_action_pressed("move_up") and is_in_trunk():
+		velocity.y = -SPEED
+		is_climbing = true
+	else:
+		velocity.y = 0
+		is_climbing = false
+	
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if velocity.y < 0:
+		current_anim_sprite.animation = "up"
+	elif velocity.y > 0:
+		current_anim_sprite.animation = "fall"
+	elif velocity.x != 0:
+		current_anim_sprite.animation = "walk"
+		current_anim_sprite.flip_h = velocity.x < 0
+	else:
+		current_anim_sprite.animation = "idle"
+		
+	current_anim_sprite.play()
+
+	move_and_slide()
+
+func enable_current_evolution():
+	current_anim_sprite.set_visible(true)
+	current_coll_shape.set_disabled(false)
+
+func set_evolution(new_evolution):
+	match new_evolution:
+		"monkey":
+			current_anim_sprite = $MonkeyAnimation
+			current_coll_shape = $MonkeyCollision
+			can_jump = true
+			can_climb = false
+		"snail":
+			current_anim_sprite = $SnailAnimation
+			current_coll_shape = $SnailCollision
+			can_jump = false
+			can_climb = true
+
+func is_in_trunk():
+	return true
