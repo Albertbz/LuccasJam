117ab02c83cd57819a2e88d69cd81d3f4339ce6e
Albert Unmack Bærentzen <202104567@post.au.dk>
2023-10-14 10:38:17 +0200

    Added monkey

---
+++ monkey.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_pressed("move_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if velocity.y < 0:
+		$AnimatedSprite2D.animation = "jump"
+	elif velocity.y > 0:
+		$AnimatedSprite2D.animation = "fall"
+	elif velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_h = velocity.x > 0
+	else:
+		$AnimatedSprite2D.animation = "idle"
+		
+	$AnimatedSprite2D.play()
+
+	move_and_slide()
+
