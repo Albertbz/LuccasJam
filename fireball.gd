901d042926806b786a09be4a397aeebf81414b6f
Luccas <luccassukul17@gmail.com>
2023-10-14 13:21:48 +0200

    made dragon

---
+++ fireball.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var direction = -1
+var speed = 200 
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		print("Player hit!")
+		queue_free()
+	elif body.get_parent().name != get_parent().name:
+		queue_free()
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	velocity.x += direction
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
